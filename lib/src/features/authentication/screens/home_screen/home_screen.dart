b3ad3bc60f8a0ae1bdbc31e912016342d3bca030
puririkesh2000 <puririkesh2000@gmail.com>
2023-07-03 16:20:37 +0545

    Map was added to homescreen  and current marker was used

--- lib/src/features/authentication/screens/home_screen/home_screen.dart
+++ lib/src/features/authentication/screens/home_screen/home_screen.dart
@@ -17,11 +17,29 @@ class _HomeScreenState extends State<HomeScreen> {
     target: LatLng(27.671333297258062, 85.33869273960579),
     zoom: 15,
   );
+
+  List<Marker> _marker = [];
+  List<Marker> _list = const [
+    Marker(
+      markerId: MarkerId('1'),
+      position: LatLng(27.671333297258062, 85.33869273960579),
+      infoWindow: InfoWindow(title: "Current Location"),
+    )
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _marker.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: GoogleMap(
+          //marker show
+          markers: Set<Marker>.of(_marker),
           initialCameraPosition: _kGooglePlex,
           mapType: MapType.normal,
           compassEnabled: false,
