1d1ee7ca9a22d8464773a4a0ebf22b77446620bc
Benjil Shrestha <benjil.191406@ncit.edu.np>
2023-07-04 22:04:39 +0545

    google signin package added

--- lib/auth/authpage.dart
+++ lib/auth/authpage.dart
@@ -1,4 +1,6 @@
 // import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 // import '../pages/call_page.dart';
 // import '../pages/registerpage.dart';
@@ -26,3 +28,24 @@
 //   return Registerpage(showLoginPage:toggleScreens);
 // }}
 // }
+
+/// google signin first try (BS)
+
+/// [GoogleAuthetication]
+Future<UserCredential?> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
