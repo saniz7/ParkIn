477af1a5180a7417f313bd7b0d405aa66f4b2949
Samyog <iamsamyogadhikari@gmail.com>
2023-07-09 12:00:17 +0545

    User details extracted to admindashboard

--- lib/src/features/authentication/screens/admin/admin_screen.dart
+++ lib/src/features/authentication/screens/admin/admin_screen.dart
@@ -1,309 +1,3 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:learn01/src/features/authentication/screens/login/login_screen.dart';
-
-// class AdminScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Admin Screen'),
-//       ),
-//       body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-//         future: FirebaseFirestore.instance
-//             .collection('users')
-//             .doc(FirebaseAuth.instance.currentUser?.uid)
-//             .get(),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             // Data is still loading
-//             return Center(child: CircularProgressIndicator());
-//           } else if (snapshot.hasData && snapshot.data?.exists == true) {
-//             // User data is available
-//             Map<String, dynamic>? userData = snapshot.data?.data();
-//             if (userData != null) {
-//               // Extract the user's email
-//               String email = userData['email'];
-//               return Column(
-//                 crossAxisAlignment: CrossAxisAlignment.stretch,
-//                 children: [
-//                   Container(
-//                     padding: EdgeInsets.all(16.0),
-//                     color: Colors.blue,
-//                     child: Text(
-//                       'Admin Screen',
-//                       style: TextStyle(
-//                         fontSize: 24,
-//                         fontWeight: FontWeight.bold,
-//                         color: Colors.white,
-//                       ),
-//                     ),
-//                   ),
-//                   Expanded(
-//                     child: Padding(
-//                       padding: EdgeInsets.all(16.0),
-//                       child: Column(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: [
-//                           Text(
-//                             'Email: $email',
-//                             style: TextStyle(fontSize: 18),
-//                           ),
-//                           SizedBox(height: 32),
-//                           Expanded(
-//                             child: GridView.count(
-//                               crossAxisCount: 2,
-//                               children: [
-//                                 ElevatedButton(
-//                                   onPressed: () {
-//                                     // Handle button press
-//                                   },
-//                                   child: Text('User'),
-//                                 ),
-//                                 ElevatedButton(
-//                                   onPressed: () {
-//                                     // Handle button press
-//                                   },
-//                                   child: Text('Places'),
-//                                 ),
-//                                 ElevatedButton(
-//                                   onPressed: () {
-//                                     // Handle button press
-//                                   },
-//                                   child: Text('Users (Manage)'),
-//                                 ),
-//                                 ElevatedButton(
-//                                   onPressed: () {
-//                                     // Handle button press
-//                                   },
-//                                   child: Text('Places (Manage)'),
-//                                 ),
-//                               ],
-//                             ),
-//                           ),
-//                           SizedBox(height: 16),
-//                           ElevatedButton(
-//                             onPressed: () {
-//                               FirebaseAuth.instance.signOut();
-//                               Navigator.pushReplacement(
-//                                 context,
-//                                 MaterialPageRoute(
-//                                   builder: (context) => LoginScreen(),
-//                                 ),
-//                               );
-//                             },
-//                             child: Text('Logout'),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               );
-//             }
-//           }
-//           // User data not found
-//           return Center(child: Text('User data not found'));
-//         },
-//       ),
-//     );
-//   }
-// }
-
-// 2
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:learn01/src/features/authentication/screens/login/login_screen.dart';
-
-// class AdminScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Admin Screen'),
-//       ),
-//       body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-//         future: FirebaseFirestore.instance
-//             .collection('users')
-//             .doc(FirebaseAuth.instance.currentUser?.uid)
-//             .get(),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             // Data is still loading
-//             return Center(child: CircularProgressIndicator());
-//           } else if (snapshot.hasData && snapshot.data?.exists == true) {
-//             // User data is available
-//             Map<String, dynamic>? userData = snapshot.data?.data();
-//             if (userData != null) {
-//               // Extract the user's email
-//               String email = userData['email'];
-//               return Column(
-//                 crossAxisAlignment: CrossAxisAlignment.stretch,
-//                 children: [
-//                   Container(
-//                     padding: EdgeInsets.all(16.0),
-//                     color: Colors.blue,
-//                     child: Text(
-//                       'Admin Screen',
-//                       style: TextStyle(
-//                         fontSize: 24,
-//                         fontWeight: FontWeight.bold,
-//                         color: Colors.white,
-//                       ),
-//                     ),
-//                   ),
-//                   Expanded(
-//                     child: Padding(
-//                       padding: EdgeInsets.all(16.0),
-//                       child: Column(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: [
-//                           Text(
-//                             'Email: $email',
-//                             style: TextStyle(fontSize: 18),
-//                           ),
-//                           SizedBox(height: 32),
-//                           Expanded(
-//                             child: GridView.count(
-//                               crossAxisCount: 2,
-//                               children: [
-//                                 ElevatedButton(
-//                                   onPressed: () {
-//                                     Navigator.push(
-//                                       context,
-//                                       MaterialPageRoute(
-//                                         builder: (context) => UserDataScreen(),
-//                                       ),
-//                                     );
-//                                   },
-//                                   child: Text('User'),
-//                                 ),
-//                                 ElevatedButton(
-//                                   onPressed: () {
-//                                     Navigator.push(
-//                                       context,
-//                                       MaterialPageRoute(
-//                                         builder: (context) =>
-//                                             ParkingDataScreen(),
-//                                       ),
-//                                     );
-//                                   },
-//                                   child: Text('Places'),
-//                                 ),
-//                                 ElevatedButton(
-//                                   onPressed: () {
-//                                     Navigator.push(
-//                                       context,
-//                                       MaterialPageRoute(
-//                                         builder: (context) =>
-//                                             UserManageScreen(),
-//                                       ),
-//                                     );
-//                                   },
-//                                   child: Text('Users (Manage)'),
-//                                 ),
-//                                 ElevatedButton(
-//                                   onPressed: () {
-//                                     Navigator.push(
-//                                       context,
-//                                       MaterialPageRoute(
-//                                         builder: (context) =>
-//                                             ParkingManageScreen(),
-//                                       ),
-//                                     );
-//                                   },
-//                                   child: Text('Places (Manage)'),
-//                                 ),
-//                               ],
-//                             ),
-//                           ),
-//                           SizedBox(height: 16),
-//                           ElevatedButton(
-//                             onPressed: () {
-//                               FirebaseAuth.instance.signOut();
-//                               Navigator.pushReplacement(
-//                                 context,
-//                                 MaterialPageRoute(
-//                                   builder: (context) => LoginScreen(),
-//                                 ),
-//                               );
-//                             },
-//                             child: Text('Logout'),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               );
-//             }
-//           }
-//           // User data not found
-//           return Center(child: Text('User data not found'));
-//         },
-//       ),
-//     );
-//   }
-// }
-
-// class UserDataScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('User Data'),
-//       ),
-//       body: Center(
-//         child: Text('User Data Screen'),
-//       ),
-//     );
-//   }
-// }
-
-// class ParkingDataScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Parking Data'),
-//       ),
-//       body: Center(
-//         child: Text('Parking Data Screen'),
-//       ),
-//     );
-//   }
-// }
-
-// class UserManageScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('User (Manage)'),
-//       ),
-//       body: Center(
-//         child: Text('User (Manage) Screen'),
-//       ),
-//     );
-//   }
-// }
-
-// class ParkingManageScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Places (Manage)'),
-//       ),
-//       body: Center(
-//         child: Text('Places (Manage) Screen'),
-//       ),
-//     );
-//   }
-// }
-
 // 3
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -366,23 +60,50 @@ class AdminScreen extends StatelessWidget {
                             child: GridView.count(
                               crossAxisCount: 2,
                               children: [
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => UserDataScreen(),
+                                StreamBuilder<
+                                    QuerySnapshot<Map<String, dynamic>>>(
+                                  stream: FirebaseFirestore.instance
+                                      .collection('users')
+                                      .snapshots(),
+                                  builder: (context, snapshot) {
+                                    if (snapshot.hasError) {
+                                      return Text('Error: ${snapshot.error}');
+                                    }
+
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return Center(
+                                          child: CircularProgressIndicator());
+                                    }
+
+                                    int userCount = snapshot.data!.docs.length;
+
+                                    return ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                UserDataScreen(),
+                                          ),
+                                        );
+                                      },
+                                      child: Column(
+                                        children: [
+                                          Text('User'),
+                                          Text('Total: $userCount'),
+                                        ],
                                       ),
                                     );
                                   },
-                                  child: Text('User'),
                                 ),
                                 ElevatedButton(
                                   onPressed: () {
                                     Navigator.push(
                                       context,
                                       MaterialPageRoute(
-                                        builder: (context) => ParkingDataScreen(),
+                                        builder: (context) =>
+                                            ParkingDataScreen(),
                                       ),
                                     );
                                   },
@@ -393,7 +114,8 @@ class AdminScreen extends StatelessWidget {
                                     Navigator.push(
                                       context,
                                       MaterialPageRoute(
-                                        builder: (context) => UserManageScreen(),
+                                        builder: (context) =>
+                                            UserManageScreen(),
                                       ),
                                     );
                                   },
--- lib/src/features/authentication/screens/admin/user_manage.dart
+++ lib/src/features/authentication/screens/admin/user_manage.dart
@@ -1,16 +1,252 @@
+// import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class UserManageScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('User (Manage)'),
+//       ),
+//       body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+//         stream: FirebaseFirestore.instance.collection('users').snapshots(),
+//         builder: (context, snapshot) {
+//           if (snapshot.hasError) {
+//             return Text('Error: ${snapshot.error}');
+//           }
+
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return Center(child: CircularProgressIndicator());
+//           }
+
+//           List<DataRow> rows = [];
+
+//           snapshot.data!.docs.forEach((doc) {
+//             Map<String, dynamic>? userData = doc.data();
+//             String username = userData?['username'] ?? '';
+//             String email = userData?['email'] ?? '';
+//             String phoneno = userData?['phoneno']?.toString() ?? '';
+//             String password = userData?['password'] ?? '';
+//             String role = userData?['role'] ?? '';
+
+//             rows.add(
+//               DataRow(
+//                 cells: [
+//                   DataCell(Text(username)),
+//                   DataCell(Text(email)),
+//                   DataCell(Text(phoneno)),
+//                   DataCell(Text(password)),
+//                   DataCell(Text(role)),
+//                   DataCell(
+//                     IconButton(
+//                       icon: Icon(Icons.edit),
+//                       onPressed: () {
+//                         // Perform edit action for the user
+//                         // Replace with your desired functionality
+//                       },
+//                     ),
+//                   ),
+//                   DataCell(
+//                     IconButton(
+//                       icon: Icon(Icons.delete),
+//                       onPressed: () {
+//                         // Perform delete action for the user
+//                         // Replace with your desired functionality
+//                       },
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             );
+//           });
+
+//           return SingleChildScrollView(
+//             scrollDirection: Axis.horizontal,
+//             child: SingleChildScrollView(
+//               scrollDirection: Axis.vertical,
+//               child: DataTable(
+//                 columns: [
+//                   DataColumn(label: Text('Username')),
+//                   DataColumn(label: Text('Email')),
+//                   DataColumn(label: Text('Phone No')),
+//                   DataColumn(label: Text('Password')),
+//                   DataColumn(label: Text('Role')),
+//                   DataColumn(label: Text('Edit')),
+//                   DataColumn(label: Text('Delete')),
+//                 ],
+//                 rows: rows,
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class UserManageScreen extends StatefulWidget {
+  @override
+  _UserManageScreenState createState() => _UserManageScreenState();
+}
 
-class UserManageScreen extends StatelessWidget {
+class _UserManageScreenState extends State<UserManageScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phonenoController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _roleController = TextEditingController();
+  bool _isEditing = false;
+  late String _userId;
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _phonenoController.dispose();
+    _passwordController.dispose();
+    _roleController.dispose();
+    super.dispose();
+  }
+
+  void _editUser(String userId, String username, String email, String phoneno,
+      String password, String role) {
+    setState(() {
+      _isEditing = true;
+      _userId = userId;
+      _usernameController.text = username;
+      _emailController.text = email;
+      _phonenoController.text = phoneno;
+      _passwordController.text = password;
+      _roleController.text = role;
+    });
+  }
+
+  void _saveUser() {
+    if (_formKey.currentState!.validate()) {
+      final updatedUser = {
+        'username': _usernameController.text.trim(),
+        'email': _emailController.text.trim(),
+        'phoneno': _phonenoController.text.trim(),
+        'password': _passwordController.text.trim(),
+        'role': _roleController.text.trim(),
+      };
+
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(_userId)
+          .update(updatedUser)
+          .then((_) {
+        setState(() {
+          _isEditing = false;
+          _userId = '';
+          _usernameController.clear();
+          _emailController.clear();
+          _phonenoController.clear();
+          _passwordController.clear();
+          _roleController.clear();
+        });
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('User updated successfully'),
+        ));
+      }).catchError((error) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Failed to update user: $error'),
+          backgroundColor: Colors.red,
+        ));
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('User (Manage)'),
       ),
-      body: Center(
-        child: Text('User (Manage) Screen'),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          List<DataRow> rows = [];
+
+          snapshot.data!.docs.forEach((doc) {
+            Map<String, dynamic>? userData = doc.data();
+            String userId = doc.id;
+            String username = userData?['username'] ?? '';
+            String email = userData?['email'] ?? '';
+            String phoneno = userData?['phoneno']?.toString() ?? '';
+            String password = userData?['password'] ?? '';
+            String role = userData?['role'] ?? '';
+
+            rows.add(
+              DataRow(
+                cells: [
+                  DataCell(Text(username)),
+                  DataCell(Text(email)),
+                  DataCell(Text(phoneno)),
+                  DataCell(Text(password)),
+                  DataCell(Text(role)),
+                  DataCell(
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        _editUser(
+                            userId, username, email, phoneno, password, role);
+                      },
+                    ),
+                  ),
+                  DataCell(
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        // Perform delete action for the user
+                        // Replace with your desired functionality
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          });
+
+          return SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: DataTable(
+                columns: [
+                  DataColumn(label: Text('Username')),
+                  DataColumn(label: Text('Email')),
+                  DataColumn(label: Text('Phone No')),
+                  DataColumn(label: Text('Password')),
+                  DataColumn(label: Text('Role')),
+                  DataColumn(label: Text('Edit')),
+                  DataColumn(label: Text('Delete')),
+                ],
+                rows: rows,
+              ),
+            ),
+          );
+        },
       ),
+      floatingActionButton: _isEditing ? _buildSaveButton() : null,
+    );
+  }
+
+  Widget _buildSaveButton() {
+    return FloatingActionButton(
+      onPressed: _saveUser,
+      child: Icon(Icons.save),
     );
   }
 }
