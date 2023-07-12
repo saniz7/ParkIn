4d237106ed65adee9391c65452d70ca6f2f3b534
puririkesh2000 <puririkesh2000@gmail.com>
2023-07-12 14:03:29 +0545

    redoing khalti intergration

---
+++ lib/src/features/authentication/screens/Khalti/khalti-home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
+
+void main() => runApp(const KhaltiPaymentApp());
+
+class KhaltiPaymentApp extends StatelessiWidget {
+const KhaltiPaymentApp({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+
+return KhaltiScope(
+est_public_key_30e12814fed64afaga7ddag2a2194aeb”,
+builder: (context, navigatorKey) {
+
+publicKey:
+
+return MaterialApp(
+navigatorKey: navigatorKey,
+supportedlocales: const [
+Locale('en', 'US'),
+Locale('ne’, "NP'),
+1,
+
+localizationsDelegates: const [
+
+Khaltilocalizations. delegate,
+1
+theme: ThemeData(
+primaryColor: const Color (@xFF56328¢c),
+appBarTheme: const AppBarTheme(
+color: Color(@xFF56328c),
