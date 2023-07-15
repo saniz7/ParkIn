385f7a3e69b0af6a9d82a7807123217167d3bebb
Benjil Shrestha <benjil.191406@ncit.edu.np>
2023-07-15 09:45:40 +0545

    animate camera feature addded

--- lib/googlescreen.dart
+++ lib/googlescreen.dart
@@ -23,11 +23,15 @@ class _HomeState extends State<Home> {
   );
 
   List<Marker> _marker = [];
-  List<Marker> _list = const [
+  final List<Marker> _list = const [
     Marker(
         markerId: MarkerId('1'),
         position: LatLng(27.700769, 85.300140),
-        infoWindow: InfoWindow(title: 'My current location'))
+        infoWindow: InfoWindow(title: 'My current location')),
+    Marker(
+        markerId: MarkerId('2'),
+        position: LatLng(27.6712723667, 85.3390844333),
+        infoWindow: InfoWindow(title: 'Ncit College'))
   ];
 
   @override
@@ -40,14 +44,26 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
-        markers: Set<Marker>.of(_marker),
-        mapType: MapType.normal,
-        myLocationEnabled: false,
-        compassEnabled: false,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          markers: Set<Marker>.of(_marker),
+          mapType: MapType.normal,
+          myLocationEnabled: false,
+          compassEnabled: false,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_disabled_outlined),
+        onPressed: () async {
+          GoogleMapController controller = await _controller.future;
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                  target: LatLng(27.6712723667, 85.3390844333), zoom: 14)));
+          setState(() {});
         },
       ),
     );
