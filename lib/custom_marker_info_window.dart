68f2bfbe0d3f8aa955976bac470835a424cfb9db
Sanij <Sanizsanij@gmail.com>
2023-09-29 00:06:00 +0545

    Fix[Map]:Marker automatic removal and added

--- lib/custom_marker_info_window.dart
+++ lib/custom_marker_info_window.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:learn01/src/features/authentication/screens/booking/bookingpage.dart';
 import 'package:learn01/src/features/authentication/screens/profile/profile_screen.dart';
 
 class CustomMarkerInfoWindow extends StatefulWidget {
@@ -16,6 +19,7 @@ class _CustomMarkerInfoWindowState extends State<CustomMarkerInfoWindow> {
       CustomInfoWindowController();
   final List<Marker> _markers = <Marker>[];
   final List<LatLng> _latLng = [];
+  StreamSubscription<QuerySnapshot<Map<String, dynamic>>>? _subscription;
 
   @override
   void initState() {
@@ -24,18 +28,32 @@ class _CustomMarkerInfoWindowState extends State<CustomMarkerInfoWindow> {
     fetchSpaceData();
   }
 
-  Future<void> fetchSpaceData() async {
-    try {
-      FirebaseFirestore firestore = FirebaseFirestore.instance;
-      QuerySnapshot<Map<String, dynamic>> querySnapshot =
-          await firestore.collection('space').get();
+  void navigateToManageScreen(BuildContext context,
+      DocumentSnapshot<Map<String, dynamic>> documentSnapshot) {
+    final spaceData = documentSnapshot.data();
+    if (spaceData != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => BookingPageSpaceScreen(spaceData: spaceData),
+        ),
+      );
+    }
+  }
+
+  void fetchSpaceData() {
+    _subscription = FirebaseFirestore.instance
+        .collection('space')
+        .where('view', isEqualTo: 'yes')
+        .snapshots()
+        .listen((querySnapshot) {
+      _markers.clear(); // Clear existing markers
+      _latLng.clear(); // Clear existing LatLngs
 
       for (QueryDocumentSnapshot<Map<String, dynamic>> document
           in querySnapshot.docs) {
-        double latitude = document['latitude']
-            as double; // Replace 'latitude' with the actual field name
-        double longitude = document['longitude']
-            as double; // Replace 'longitude' with the actual field name
+        double latitude = document['latitude'] as double;
+        double longitude = document['longitude'] as double;
 
         _latLng.add(LatLng(latitude, longitude));
 
@@ -63,34 +81,75 @@ class _CustomMarkerInfoWindowState extends State<CustomMarkerInfoWindow> {
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
                           'Place Info',
-                          maxLines: 1,
-                          overflow: TextOverflow.fade,
-                          softWrap: false,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Name: ${document['spacename']}',
-                        ),
+                      Divider(),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Name: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            '${document['spacename']}',
+                          ),
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Description: ${document['description']}',
-                        ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Description: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            '${document['description']}',
+                          ),
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Available Space:${document['availablespace'].toString()}',
-                        ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Available Space: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            '${document['availablespace'].toString()}',
+                          ),
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Rate:${document['rate'].toString()}',
-                        ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Rate: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            '${document['rate'].toString()}',
+                          ),
+                        ],
                       ),
                     ],
                   ),
@@ -102,9 +161,13 @@ class _CustomMarkerInfoWindowState extends State<CustomMarkerInfoWindow> {
         );
       }
       setState(() {});
-    } catch (e) {
-      print('Error fetching data: $e');
-    }
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
   }
 
   @override
@@ -142,28 +205,57 @@ class _CustomMarkerInfoWindowState extends State<CustomMarkerInfoWindow> {
               offset: 35,
             ),
             Positioned.fill(
-                child: DraggableScrollableSheet(
-              maxChildSize: 0.7,
-              minChildSize: 0.3,
-              builder: (_, controller) {
-                return Material(
-                  elevation: 10,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(20),
-                  ),
-                  color: Colors.white,
-                  child: ListView.builder(
-                    controller: controller,
-                    itemCount: 20,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text('Index :$index'),
-                      );
-                    },
-                  ),
-                );
-              },
-            ))
+              child: DraggableScrollableSheet(
+                maxChildSize: 0.7,
+                minChildSize: 0.3,
+                builder: (_, controller) {
+                  return Material(
+                    elevation: 10,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: Colors.white,
+                    child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                      stream: FirebaseFirestore.instance
+                          .collection('space')
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return CircularProgressIndicator();
+                        } else if (snapshot.hasData) {
+                          List<QueryDocumentSnapshot<Map<String, dynamic>>>
+                              documents = snapshot.data!.docs;
+                          if (documents.isNotEmpty) {
+                            return ListView.builder(
+                              controller: controller,
+                              itemCount: documents.length,
+                              itemBuilder: (context, index) {
+                                Map<String, dynamic>? spaceData =
+                                    documents[index].data();
+                                if (spaceData != null) {
+                                  return ListTile(
+                                    title: Text(spaceData['spacename']),
+                                    subtitle: Text(spaceData['description']),
+                                    onTap: () => navigateToManageScreen(
+                                      context,
+                                      documents[index],
+                                    ),
+                                    // You can customize the list item as needed
+                                  );
+                                }
+                                return const SizedBox.shrink();
+                              },
+                            );
+                          }
+                        }
+                        return const Text('No parking spaces found');
+                      },
+                    ),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
